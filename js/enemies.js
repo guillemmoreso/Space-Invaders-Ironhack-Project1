c636394673806c419b8822af59b9a0cc714da8d7
Guillem Moreso <guillem.moreso@gmail.com>
2019-07-08 11:26:04 +0200

    UPDATE refactor funciones

--- js/enemies.js
+++ js/enemies.js
@@ -1,7 +1,6 @@
 "use strict";
 class Enemy {
   constructor(x, y, width, height, ctx) {
-    this.game = game;
     this.x = x;
     this.y = y;
     this.width = width;
@@ -14,7 +13,11 @@ class Enemy {
     this.yVelocidad = 3;
   }
   draw() {
-    // this.ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  clear() {
+    this.ctx.clearRect(this.x, this.y, this.width, this.height);
   }
   shiftDown() {
     this.y += this.yVelocidad;
@@ -25,7 +28,6 @@ class Enemy {
   }
   moveRight() {
     this.x += this.xVelocidad;
-    console.log(game.gameWidth);
     // if (this.x === 500) {
     //   this.xVelocidad = 0;
     //   this.shiftDown();
--- js/game.js
+++ js/game.js
@@ -25,7 +25,8 @@ class Game {
 
   _drawEnemies() {
     this.enemies.forEach(enemy => {
-      this.ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+      enemy.clear();
+
       if (enemy.direction) {
         enemy.moveRight();
       } else {
@@ -34,10 +35,12 @@ class Game {
 
       if (enemy.x > 750 || enemy.x < 0) {
         this.enemies.forEach(enemyY => {
-          enemyY.y += 100;
+          enemyY.y += 50;
           enemyY.direction = !enemyY.direction;
         });
       }
+
+      enemy.draw();
     });
   }
 
@@ -69,7 +72,7 @@ class Game {
     for (let i = 0; i < 30; i++) {
       let x = 20 + (i % 8) * 60;
       let y = 20 + (i % 3) * 60;
-      this.enemies.push(new Enemy(x, y, 20, 20));
+      this.enemies.push(new Enemy(x, y, 20, 20, this.ctx));
     }
   }
 
@@ -113,14 +116,12 @@ class Game {
   window.requestAnimationFrame(this._update.bind(this));
 }
   }
-
 pause() {
   if (this.intervalGame) {
     window.cancelAnimationFrame(this.intervalGame);
     this.intervalGame = undefined;
   }
 }
-
 start() {
   // bucle infinito que se encarga de pintar el canvas
   this._assignControlsToKeys();
